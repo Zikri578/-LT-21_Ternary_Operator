f42e29df200a0d75ec16569a1801fa76d7341c13
MuhammadZikri <zikrim577@gmail.com>
2023-01-16 14:13:18 +0700

    Javascript - Ternary Operator

---
+++ ternary.js
@@ -0,0 +1,8 @@
+/**
+ * Menggunakan Ternary Operation
+ */
+// membuat variabel angka 
+const angka = 75;
+const ucapan = angka >= 80 ? "Selamat Anda Lulus" : "Anda Tidak Lulus";
+
+console.info(ucapan);
